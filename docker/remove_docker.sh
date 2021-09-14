21eba9fd4894e4dea7ecf9546da72927cea9d642
Rafael Ribeiro <rafael.alves.ribeiro@gmail.com>
2021-09-14 17:03:29 -0300

    Update remove_docker.sh

--- docker/remove_docker.sh
+++ docker/remove_docker.sh
@@ -2,7 +2,10 @@
 
 # Remove completamente o Docker do sistema (Ubuntu)
 sudo service docker stop
-sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
+sudo apt-get purge -y docker.io
+sudo apt-get purge -y docker-engine
+sudo apt-get purge -y docker-ce
+sudo apt-get purge -y docker-ce-cli
 sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
 sudo rm -rf /var/lib/docker /etc/docker
 sudo rm /etc/apparmor.d/docker
